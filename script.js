bc46c8e758b0094d0f9fc50f795a600ea13c4de1
Lattapan <noey.rachata@gmail.com>
2022-11-21 21:15:00 +0700

    Refactor JS code

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
+/** @format */
+
 //Close and open sidebars
+const sideBarNav = document.querySelector(".sidebar");
+const toggleMenu = document.querySelector(".fa-bars");
+
+const searchBarNav = document.querySelector(".search-bar");
+const searchMenu = document.querySelector(".fa-magnifying-glass");
+
 function closeSidebar(sidebar, icon) {
+  icon.addEventListener("click", () => {
+    const visibilityNav = sidebar.getAttribute("data-visible");
 
-    icon.addEventListener('click', () => {
-        const visibilityNav = sidebar.getAttribute('data-visible');
-    
-        if (visibilityNav === 'false') {
-            sidebar.setAttribute('data-visible', true);
-        } else if (visibilityNav === 'true') {
-            sidebar.setAttribute('data-visible', false);
-        }
-    });
+    if (visibilityNav === "false") {
+      sidebar.setAttribute("data-visible", true);
+    } else if (visibilityNav === "true") {
+      sidebar.setAttribute("data-visible", false);
+    }
+  });
 }
-
-const sideBarNav = document.querySelector('.sidebar')
-const toggleMenu = document.querySelector('.fa-bars')
 
 closeSidebar(sideBarNav, toggleMenu);
 
-
-const searchBarNav = document.querySelector('.search-bar')
-const searchMenu = document.querySelector('.fa-magnifying-glass')
-
 closeSidebar(searchBarNav, searchMenu);
 
-
 //Shrinking and Expanding navigation bar on scroll
-window.onscroll = function () { scrollFunction() };
+const mastHead = document.querySelector(".masthead").style;
+const logoImg = document.querySelector(".logo-img").style;
+const hiddenNav = document.querySelector(".scroll-hidden-nav").style;
+
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-    if (screen.width > 600 && document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.querySelector('.masthead').style.height = '5em';
-        document.querySelector('.masthead').style.transition = '350ms';
-        document.querySelector('.logo-img').style.transition = '350ms';
-        document.querySelector('.logo-img').style.width = '25rem';
-        document.querySelector('.scroll-hidden-nav').style.display = 'none';
-    } else if (screen.width > 600) {
-        document.querySelector('.masthead').style.height = '10em';
-        document.querySelector('.logo-img').style.width = '40rem';
-        document.querySelector('.scroll-hidden-nav').style.display = 'flex';
-    }
+  if (
+    (screen.width > 600 && document.body.scrollTop > 50) ||
+    document.documentElement.scrollTop > 50
+  ) {
+    mastHead.height = "5em";
+    mastHead.transition = "350ms";
+    logoImg.transition = "350ms";
+    logoImg.width = "25rem";
+    hiddenNav.display = "none";
+  } else if (screen.width > 600) {
+    mastHead.height = "10em";
+    logoImg.width = "40rem";
+    hiddenNav.display = "flex";
+  }
 }
-
-
